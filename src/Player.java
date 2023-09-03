303132f874abb82555cae82372d86a4a0314b40b
SANDHYA  SHEVATRE <141001905+sandhyashevatre@users.noreply.github.com>
2023-09-03 15:43:02 +0530

    Add files via upload

---
+++ src/Player.java
@@ -0,0 +1,35 @@
+package Play_bluff;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Player {
+    private final String name;
+    private final List<Card> hand;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+
+    public void removeCards(List<Card> cards) {
+        hand.removeAll(cards);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/Table.java
@@ -0,0 +1,25 @@
+package Play_bluff;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Table {
+    private final Stack<Card> cards;
+
+    public Table() {
+        cards = new Stack<>();
+    }
+
+    public void addCards(List<Card> newCards) {
+        cards.addAll(newCards);
+    }
+
+    public List<Card> revealCards() {
+        return new ArrayList<>(cards);
+    }
+
+    public void clear() {
+        cards.clear();
+    }
+}
